7e369e636fb8f5db75d5e8434c46a70c36e3c3c2
11Victor <valencioalcantara@outlook.com>
2021-09-18 15:34:31 -0300

    18.09.21 - Adicionando o framework Three.js

---
+++ THREE.JS/scenes/scene01.js
@@ -0,0 +1,20 @@
+const geometry = new THREE.BoxBufferGeometry();
+const material = new THREE.MeshLambertMaterial(
+    {color: 0xfc0303}
+);
+
+const cube = new THREE.Mesh(
+    geometry, material
+);
+
+scene.add(cube);
+
+
+
+renderer.setAnimationLoop(() => {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+});
+
---
+++ THREE.JS/setups/setup01.js
@@ -0,0 +1,27 @@
+const options = {
+    targetSelector: '#scene',
+    width: 800, height: 600,
+    backgroundColor: 0xeaff47
+}
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(
+    options.width, options.height
+);
+
+document.querySelector(
+    options.targetSelector)
+    .appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(
+    options.backgroundColor
+);
+
+
+const camera = new THREE.PerspectiveCamera(
+    50, options.width / options.height
+);
+
+camera.position.z = 5;
